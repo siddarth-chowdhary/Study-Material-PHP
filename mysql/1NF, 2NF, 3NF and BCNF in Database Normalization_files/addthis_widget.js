0df191baeecdd526fb0c49d43f760742358479d0
siddarthchowdhary <siddarth.chowdhary@osscube.com>
2014-07-05 13:26:33 +0530

    changes

---
+++ mysql/1NF, 2NF, 3NF and BCNF in Database Normalization_files/addthis_widget.js
@@ -0,0 +1,182 @@
+<html>
+		<head>
+		<meta content="no-cache" http-equiv="cache-control">
+		<meta content="no-cache" http-equiv="pragma">
+		<meta content="-1" http-equiv="expires">
+		<title>NEBERO: Access Denied</title>
+		<style type="text/css">
+		html
+		{
+			background-color:	#F5f5dc;
+			font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+			font-size: 12px;
+		}
+		form
+		{
+			width:70%;
+			margin:0px auto;
+			color:#444444;
+		}
+		fieldset
+		{
+			width:80%;	
+			margin:0px auto;
+			background-color: #FFFccc;
+			border-radius:25px;
+			-moz-border-radius: 25px;
+			-webkit-border-radius: 25px;
+			-khtml-border-radius: 25px;
+		}
+		legend
+		{
+			background-color:	#cc0000;
+			font-size: 16px;
+			font-weight: bold;
+			height:20px;
+			color: #ffffff;
+			margin-left: 50px;
+			
+			border-radius:7px;
+			-moz-border-radius: 7px;
+			-webkit-border-radius: 7px;
+			-khtml-border-radius: 7px;
+			padding:4px;
+		}
+		.logo
+		{
+			height: 10%;
+			font-family: Arial, Helvetica, sans-serif;
+			font-size: 34px;
+			font-weight: bold;
+			color: #cc0000;
+			text-align: center;
+			//text-decoration: underline;
+		}
+		table 
+		{ 
+			empty-cells: show; 
+		}
+		table.display 
+		{
+			margin: 0 auto;
+			//clear: both;
+			width: 90%;
+		}
+		td
+		{
+			text-align: left;
+			padding:5px;
+			color:#444444;
+			font-size:14px;
+		}
+		th
+		{
+			text-align: left;
+			padding:5px;
+			color:#444444;
+			font-size:14px;
+			width: 20%;
+		}
+		table.display tr.odd 
+		{
+			background-color: #F5f5dc;
+		}
+		
+		table.display tr.even 
+		{
+			background-color: #eee8aa;
+		}
+		.copy
+		 {
+		 	width:98%;
+		 	height:20px;
+		 	text-align:right;
+		 	font-size:11px;
+		 	padding:4px;
+		 }
+		p
+		{
+			text-align: center;
+			color: #cc0000;
+		}
+		.button
+		{
+			text-align: center;
+		}
+		</style>
+		<script language="javascript">
+			function showhidefield() 
+				{
+			    if (document.error_page.fb.checked) 
+			    {
+		        	//alert("Checkbox was checked.");
+		        	 document.getElementById("hidearea").style.visibility = "visible";
+		    	 }
+		    	 else 
+		    	 {
+			        //alert("Checkbox wasn't checked.");
+			        document.getElementById("hidearea").style.visibility = "hidden";
+		   	 }
+		     	}
+		  </script>
+		</head>
+		<body>
+	   <form action="/guard/index.php" method="post" name="error_page" id="error_page">
+	   <div class="logo">NEBERO<sup>®</sup></div>
+		<fieldset><legend>Access Denied Site Rated :- InstantMessaging</legend><table align="center" cellpadding="1" cellspacing="0" class="display" border="0"><tr class="odd">
+						<th>User</th><td></td>				
+				</tr>
+				<tr class="even">
+						<th>Group</th><td></td>				
+				</tr>
+				<tr class="odd">
+						<th>IP Address</th><td>203.122.33.130</td>				
+				</tr>
+				<tr class="even">
+						<th>Url</th><td>http://s7.addthis.com/js/300/addthis_widget.js</td>				
+				</tr>
+				<tr class="odd">
+						<th>Policy</th><td></td>				
+				</tr>
+				<tr class="even">
+						<th>Date</th><td>2014-07-05 01:24:51 PM</td>
+				</tr>
+			</table>
+			</fieldset>
+			<div class="copy"> &copy; 2012 | All Rights Reserved by Nebero Systems (P) Ltd.<br>
+				<a href="http://www.nebero.com" target="_blank" >Powered By NEBERO</a>		
+			</div>
+			
+			<hr><div id="hidearea" style="visibility:hidden;">
+			<fieldset>
+				<legend>FEEDBACK</legend>
+					<table align="center" cellpadding="1" cellspacing="0" class="display" border="0">
+					<tr class="odd">
+							<th>Url</th><td><input type="text" name="url" id="url" value="http://s7.addthis.com/js/300/addthis_widget.js" readonly="readonly" size="55" /></td>				
+					</tr>
+					<tr class="even">
+							<th>Date</th><td><input type="text" name="date" id="date" value="2014-07-05 01:24:51 PM" readonly="readonly" /></td>				
+					</tr>
+					<tr class="odd">
+							<th>Request By User</th><td><input type="text" name="user" id="user" value="" readonly="readonly" /></td>				
+					</tr>
+					<tr class="even">
+							<th>IP Address</th><td><input type="text" name="ip" id="ip" value="203.122.33.130" readonly="readonly" /></td>				
+					</tr>	
+					<tr class="odd">
+							<th>Reason</th><td><textarea maxlength="200" cols="30" rows="5" name="reason"> </textarea></td>				
+					</tr>
+					<tr>
+						<td colspan="2"><input type="submit" name="req" value="Request" class="button"/></td>					
+					</tr>
+				</table>	
+				<input type="hidden" name="pol" value="">
+				<input type="hidden" name="group" value="">
+				<input type="hidden" name="class" value="InstantMessaging">
+				
+			</fieldset>
+			</div>
+		
+			</form>
+			</body>
+			</html>
