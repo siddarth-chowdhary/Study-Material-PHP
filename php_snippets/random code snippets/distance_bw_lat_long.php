7a5880d3a78a33c4d676f0fb813b32b79db3e556
siddarth-chowdhary <siddarthchowdhary@gmail.com>
2014-12-22 14:19:45 +0530

    Changes

---
+++ php_snippets/random code snippets/distance_bw_lat_long.php
@@ -0,0 +1,20 @@
+<?php
+function distance($lat1, $lng1, $lat2, $lng2, $miles = true)
+{
+	$pi80 = M_PI / 180;
+	$lat1 *= $pi80;
+	$lng1 *= $pi80;
+	$lat2 *= $pi80;
+	$lng2 *= $pi80;
+
+	$r = 6372.797; // mean radius of Earth in km
+	$dlat = $lat2 - $lat1;
+	$dlng = $lng2 - $lng1;
+	$a = sin($dlat / 2) * sin($dlat / 2) + cos($lat1) * cos($lat2) * sin($dlng / 2) * sin($dlng / 2);
+	$c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+	$km = $r * $c;
+
+	return ($miles ? ($km * 0.621371192) : $km);
+}
+
+var_dump(distance(28.6100, 77.2300,18.9750, 72.8258));
